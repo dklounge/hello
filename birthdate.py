3d816b611f90a454673b7fb653918e4825d2ab49
David Kim <daviddarden11@gmail.com>
2013-12-28 22:29:22 -0800

    handling escape

--- birthdate.py
+++ birthdate.py
@@ -1,9 +1,9 @@
 class Birthdate:
-    """ birthday validation functions for inputs month, day, year """
-    def __init__(self, day, month, year):
-        self.day = day
-        self.month = month
-        self.year = year
+		""" birthday validation functions for inputs month, day, year """
+		def __init__(self, day, month, year):
+				self.day = day
+				self.month = month
+				self.year = year
 
-    def birthday (day, month, year):
-        print "your birthday is ", year
+		def birthday (day, month, year):
+				print "your birthday is ", year
--- hellowiki.py
+++ hellowiki.py
@@ -5,13 +5,13 @@ form = """
   What is your birthday?
   <br>
   <label> Month
-    <input type="text" name="month" value="%(month)s">
+	<input type="text" name="month" value="%(month)s">
   </label>
   <label> Date
-    <input type="text" name="day" value="%(day)s">
+	<input type="text" name="day" value="%(day)s">
   </label>
   <label> Year
-    <input type="text" name="year" value="%(year)s">
+	<input type="text" name="year" value="%(year)s">
   </label>
   <div style="color:red";>%(error)s</div>
   <br>
@@ -21,78 +21,78 @@ form = """
 """
 
 class MainPage(webapp2.RequestHandler):
-    def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error,
-                                        "month": escape_html(month),
-                                        "day": escape_html(day),
-                                        "year": escape_html(year)})
+	def write_form(self, error="", month="", day="", year=""):
+		self.response.out.write(form % {"error": error,
+										"month": escape_html(month),
+										"day": escape_html(day),
+										"year": escape_html(year)})
 
-    def get(self):
-        self.write_form()
+	def get(self):
+		self.write_form()
 
-    def post(self):
-        user_month = self.request.get('month')
-        user_day = self.request.get('day')
-        user_year = self.request.get('year')
+	def post(self):
+		user_month = self.request.get('month')
+		user_day = self.request.get('day')
+		user_year = self.request.get('year')
 
-        month = valid_month(user_month)
-        day = valid_day(user_day)
-        year = valid_year(user_year)
+		month = valid_month(user_month)
+		day = valid_day(user_day)
+		year = valid_year(user_year)
 
-        if not (month and day and year):
-            self.write_form("That doesn't look right.", user_month, user_day, user_year)
-        else:
-            self.redirect("/thanks")
+		if not (month and day and year):
+			self.write_form("That doesn't look right.", user_month, user_day, user_year)
+		else:
+			self.redirect("/thanks")
 
 class ThanksHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("Thanks for telling us your birthday!")
+	def get(self):
+		self.response.out.write("Thanks for telling us your birthday!")
 
 app = webapp2.WSGIApplication([('/', MainPage), ('/thanks', ThanksHandler)], debug=True)
 
 months = ['January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December']
+		  'February',
+		  'March',
+		  'April',
+		  'May',
+		  'June',
+		  'July',
+		  'August',
+		  'September',
+		  'October',
+		  'November',
+		  'December']
 
 months_abb = dict((m[:3].lower(), m) for m in months)
 
 def valid_month(month):
-    if month:
-        short_month = month[:3].lower()
-        return months_abb.get(short_month)
-    else:
-        return None
+	if month:
+		short_month = month[:3].lower()
+		return months_abb.get(short_month)
+	else:
+		return None
 
 def valid_day(day):
-    if day.isdigit():
-        day = int(day)
-        # Assume all months have 31 days
-        if day in range(1, 32):
-            return day
-        else:
-            return None
+	if day.isdigit():
+		day = int(day)
+		# Assume all months have 31 days
+		if day in range(1, 32):
+			return day
+		else:
+			return None
 
 def valid_year(year):
-    if year.isdigit():
-        year = int(year)
-        if year in range (1900, 2021):
-            return year
-        else:
-            return None
+	if year.isdigit():
+		year = int(year)
+		if year in range (1900, 2021):
+			return year
+		else:
+			return None
 
 def escape_html(s):
-    for (i, o) in (("&", "&amp;"),
-                  (">", "&gt;"),
-                  ("<", "&lt;"),
-                  ('"',"&quote;")):
-        s = s.replace(i, o)
-    return s
+	for (i, o) in (("&", "&amp;"),
+				  (">", "&gt;"),
+				  ("<", "&lt;"),
+				  ('"',"&quote;")):
+		s = s.replace(i, o)
+	return s
